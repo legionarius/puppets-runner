824f9d85f83e8738c618917152ac4131b8cfc9b9
Romain Marino <nahaulu@gmail.com>
2021-05-23 11:13:30 +0000

    Resolve "Refacto génération mauvais bloc"

--- src/ActionTypeGenerator.cpp
+++ src/ActionTypeGenerator.cpp
@@ -8,47 +8,35 @@ using namespace godot;
 
 ActionTypeGenerator::ActionTypeGenerator() {
 	lastActionType = ActionType::RUN;
-	difficulty = 1;
+	rng = RandomNumberGenerator()._new();
+	rng->randomize();
+}
+
+ActionTypeGenerator::~ActionTypeGenerator() {
+	rng->free();
 }
 
 std::list<std::array<std::list<ActionType>, 3>> ActionTypeGenerator::generate_actions(size_t nbBloc, size_t nbAction) {
 	std::list<std::array<std::list<ActionType>, 3>> listBlocs;
-
 	for (int i = 0; i < nbBloc; i++) {
-		if(i == 0){
-			listBlocs.push_back(_generate_starting_blocs(nbAction));
-		} else {
-			listBlocs.push_back(_generate_blocs(nbAction));
-		}
+		listBlocs.push_back(_generate_blocs(nbAction));
 	}
-
 	return listBlocs;
 }
 
 std::array<std::list<ActionType>, 3> ActionTypeGenerator::_generate_blocs(size_t nbAction) {
 	std::array<std::list<ActionType>, 3> blocs;
-
-	// Generate 1 valid bloc
-	std::list<ActionType> validBloc = _generate_valid_bloc(nbAction);
-	blocs[0] = validBloc;
-
-	// Generate 2 wrong bloc
-	for (int a = 1; a < 3; a++) {
-		std::list<ActionType> wrongBloc = _generate_wrong_bloc(validBloc);
-		blocs[a] = wrongBloc;
+	blocs[0] = _generate_valid_bloc(nbAction);
+	while (blocs[1] == blocs[2] || blocs[0] == blocs[1] || blocs[0] == blocs[2]) {
+		blocs[1] = _generate_bad_bloc(blocs[0]);
+		blocs[2] = _generate_bad_bloc(blocs[0]);
 	}
-
 	return blocs;
 }
 
-std::array<std::list<ActionType>, 3> ActionTypeGenerator::_generate_starting_blocs(size_t nbAction){
-	std::array<std::list<ActionType>, 3> blocs;
-	std::list<ActionType> runBloc;
-
-	for (int a = 0; a < nbAction; a++) {
-		runBloc.push_back(ActionType::RUN);
-	}
-	return {runBloc, runBloc, runBloc};
+std::array<std::list<ActionType>, 3> ActionTypeGenerator::generate_starting_actions() {
+	std::list<ActionType> runBloc = { ActionType::RUN, ActionType::RUN, ActionType::RUN };
+	return { runBloc, runBloc, runBloc };
 }
 
 std::list<ActionType> ActionTypeGenerator::_generate_valid_bloc(size_t nbAction) {
@@ -61,38 +49,38 @@ std::list<ActionType> ActionTypeGenerator::_generate_valid_bloc(size_t nbAction)
 	return actionTypes;
 }
 
-std::list<ActionType> ActionTypeGenerator::_generate_wrong_bloc(std::list<ActionType> validBloc) {
-	std::list<ActionType> actionTypes = validBloc;
-
-	for (auto actionTypeIt = actionTypes.begin(); actionTypeIt != actionTypes.end(); ++actionTypeIt) {
-		auto wrongActionType = _generate_next_wrong_actiontype(*actionTypeIt);
-		*actionTypeIt = wrongActionType;
+std::list<ActionType> ActionTypeGenerator::_generate_bad_bloc(std::list<ActionType> bloc) {
+	std::list<ActionType>::iterator blocIterator = bloc.begin();
+	for (size_t i = 0; i < bloc.size(); i++) {
+		if (need_to_change_action_type_at_index(i, bloc.size())) {
+			*std::next(blocIterator, i) = _generate_next_bad_actiontype(*std::next(blocIterator, i));
+		}
 	}
-
-	return actionTypes;
+	return bloc;
 }
 
 ActionType ActionTypeGenerator::_generate_next_valid_actiontype(ActionType lastActionType) {
 	ActionType action;
-	switch (lastActionType) {
-		case ActionType::RUN:
-			action = ActionType(rand() % 3);
-			break;
-		case ActionType::JUMP:
-			action = ActionType::RUN;
-			break;
-		case ActionType::JUMP_OVER:
-			action = ActionType::RUN;
-			break;
+	// Exclude JUMP ActionType
+	int actionTypeChoices[2] = { 0, 2 };
+
+	// Prevent succession of JUMP generation
+	if (lastActionType == ActionType::JUMP) {
+		action = ActionType(actionTypeChoices[0]);
+	} else {
+		action = ActionType(rng->randi_range(0, 2));
 	}
+
 	return action;
 }
 
-ActionType ActionTypeGenerator::_generate_next_wrong_actiontype(ActionType actionType) {
+ActionType ActionTypeGenerator::_generate_next_bad_actiontype(ActionType actionType) {
 	ActionType action;
+	int64_t actionTypeIndex;
 	switch (actionType) {
 		case ActionType::RUN:
-			action = ActionType(rand() % 3);
+			actionTypeIndex = rng->randi_range(1, 2);
+			action = ActionType(actionTypeIndex);
 			break;
 		case ActionType::JUMP:
 			action = ActionType::JUMP_OVER;
@@ -104,10 +92,21 @@ ActionType ActionTypeGenerator::_generate_next_wrong_actiontype(ActionType actio
 	return action;
 }
 
-void ActionTypeGenerator::set_selected_bloc(std::list<ActionType> actions) {
-	blocSelected = actions;
+bool ActionTypeGenerator::need_to_change_action_type_at_index(int64_t index, int64_t nbActionInBloc) {
+	real_t randomPercentage = rng->randi_range(1, 100);
+	return randomPercentage <= exp(normalize_to_specific_range(weight_formula(index, nbActionInBloc)));
 }
 
-void ActionTypeGenerator::set_difficulty(int i) {
-	difficulty = i;
+real_t ActionTypeGenerator::weight_formula(int64_t index, int64_t nbActionInBloc) {
+	return 3/nbActionInBloc + index;
+}
+
+real_t ActionTypeGenerator::normalize_to_specific_range(real_t value) {
+	real_t max_action_in_bloc = 5;
+	real_t min_action_in_bloc = 3;
+	real_t desired_min = 2;
+	real_t desired_max = 4;
+	real_t actual_max = weight_formula(max_action_in_bloc-1, max_action_in_bloc);
+	real_t actual_min = weight_formula(0, min_action_in_bloc);
+	return desired_min + (value - actual_min) * (desired_max - desired_min) / (actual_max - actual_min);
 }
